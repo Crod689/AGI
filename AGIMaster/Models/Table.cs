ff8bd72be09d7202bcf46b3434aff3a128003ff5
Cristobal Rodriguez <cristobal.rodriguez@mavs.uta.edu>
2014-11-23 20:59:51 -0600

    Ok Everything is Working now

--- AGIMaster/Models/Table.cs
+++ AGIMaster/Models/Table.cs
@@ -11,47 +11,20 @@ namespace AGIMaster.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     
     public partial class Table
     {
         public int Id { get; set; }
-
-        [Required]
         public string Username { get; set; }
-
-        [Required]
-        [ValidatePasswordLength]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
-
-        [Required]
         public string CompanyName { get; set; }
-
-        [Required]
         public string CompanyAddress { get; set; }
-
-        [Required]
         public string PrimaryContactName { get; set; }
-
-        [Required]
-        [DataType(DataType.EmailAddress)]
         public string PrimaryEmailAddress { get; set; }
-
-        [Required]
         public string PrimaryPhone { get; set; }
-
-        [Required]
         public string SecondaryContactName { get; set; }
-
-        [Required]
-        [DataType(DataType.EmailAddress)]
         public string SecondaryEmailAddress { get; set; }
-
-        [Required]
         public string SecondaryPhone { get; set; }
-
-        [Required]
         public string ShippingAddress { get; set; }
         public Nullable<System.DateTime> CreationDate { get; set; }
         public string Roles { get; set; }
